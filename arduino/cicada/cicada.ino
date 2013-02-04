c9fd864cb3ea5a593c681856935a7221be9d0c5c
Adam DePrince <deprince@googlealumni.com>
2013-02-03 23:36:59 -0500

    Add preliminary WIFI code.  Doesn't work yet

--- arduino/cicada/cicada.ino
+++ arduino/cicada/cicada.ino
@@ -1,8 +1,51 @@
+
+
+#include <WiShield.h>
+#define UIP_UDP 1
+#include "uip.h"
+
+#include <string.h>
+
+#include "config.h"
+
+#define WIRELESS_MODE_INFRA     1
+#define WIRELESS_MODE_ADHOC     2
+
+// Wireless confuiguration parameters ----------------------------------------
+unsigned char local_ip[] = {192,168,1,2};       // IP address of WiShield
+unsigned char gateway_ip[] = {192,168,1,1};     // router or gateway IP address
+unsigned char subnet_mask[] = {255,255,255,0};  // subnet mask for the local network
+const prog_char ssid[] PROGMEM = {"ASYNCLABS"};         // max 32 bytes
+
+unsigned char security_type = 0;        // 0 - open; 1 - WEP; 2 - WPA; 3 - WPA2
+
+// WPA/WPA2 passphrase
+const prog_char security_passphrase[] PROGMEM = {"12345678"};   // max 64 characters
+
+// WEP 128-bit keys
+// sample HEX keys
+prog_uchar wep_keys[] PROGMEM = {       0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d,   // Key 0
+                                                                        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,   // Key 1
+                                                                        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,   // Key 2
+                                                                        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00    // Key 3
+                                                                };
+
+// setup the wireless mode
+// infrastructure - connect to AP
+// adhoc - connect to another WiFi device
+unsigned char wireless_mode = WIRELESS_MODE_INFRA;
+
+unsigned char ssid_len;
+unsigned char security_passphrase_len;
+
+
+
 #define LED 7
 #define SENSOR 0
 #define SAMPLES 4000
-int i;
-int bug=0;
+int i,j;
+int yes=0, no=0;
+long transmission_delay;
 float sum = 0;
 float icewater = 916;
 float bodytemp = 604 - icewater;
@@ -30,30 +73,15 @@ void setup() {
 }
 
 void loop() {
+  yes=0;
+  no=0;
 
-
-  sbi(ADCSRA,ADPS2) ;
-  sbi(ADCSRA,ADPS1) ;
-  sbi(ADCSRA,ADPS0) ;
-
-  sum = 0;
-  for(i=0;i<8;i++)
-    sum += analogRead(SENSOR);
-   sum /= 8;
-  // read the value from sensor (0)
-                       // returns 0-1023
-
-  sum -= icewater;
-  sum *= 37.0;
-  sum /= bodytemp;
-  // print value to serial port
-  
-
-  sbi(ADCSRA,ADPS2) ;
+ sbi(ADCSRA,ADPS2) ;
   cbi(ADCSRA,ADPS1) ;
   cbi(ADCSRA,ADPS0) ;
-
-
+ 
+  transmission_delay = millis() + 5 * 1000; // 15 minutes in ms
+  while(millis() < transmission_delay) {
   coeff = 2 * cos(2*M_PI*7000/freq);
 
   s = 0;
@@ -96,19 +124,55 @@ void loop() {
   sbi(ADCSRA,ADPS1) ;
   sbi(ADCSRA,ADPS0) ;
 
-  bug = 0;
   digitalWrite(LED, LOW);
   if (((power_7000hz / 10) > power_4000hz) && (power_7000hz > 100000) && (power_4000hz > 1000)) {
      digitalWrite(LED, HIGH); 
-     bug = 1;
-  }
+     yes += 1;
+  } else no += 1;
 
-  Serial.print(sum);
-  Serial.print(":");
+
   Serial.print(power_7000hz);
   Serial.print(":");
   Serial.print(power_4000hz);
   Serial.print(":");
-  Serial.println(bug);
+  Serial.print(yes);
+  Serial.print("/");
+  Serial.println(no);
   freq = SAMPLES / start;
+  delay(500);
+  }
+    sbi(ADCSRA,ADPS2) ;
+  sbi(ADCSRA,ADPS1) ;
+  sbi(ADCSRA,ADPS0) ;
+
+  sum = 0;
+  for(i=0;i<8;i++)
+    sum += analogRead(SENSOR);
+   sum /= 8;
+  // read the value from sensor (0)
+                       // returns 0-1023
+
+  sum -= icewater;
+  sum *= 37.0;
+  sum /= bodytemp;
+  // print value to serial port
+  
+  Serial.print("Temperature: ");
+  Serial.print(sum);
+  Serial.println("C");
+
+{
+        uip_ipaddr_t addr;
+        struct uip_udp_conn *c;
+        int temp = sum * 10;
+        char str[50];
+        uip_ipaddr(&addr, 209,20.80,141);
+        c = uip_udp_new(&addr, (u16_t)HTONS(17));
+       
+        sprintf(str,"cicada\t1\t%dt\%d\t%d\n",temp, yes, no); 
+
+        memcpy(uip_appdata, str, strlen(str));
+        uip_send((const char*)uip_appdata, strlen(str));
+        uip_udp_remove(c);
+}
 }
