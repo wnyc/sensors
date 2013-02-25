b627851fbadc973cf46e58c0e9159e4ec24710c7
Adam DePrince <deprince@googlealumni.com>
2013-02-25 15:09:32 -0500

    Add documentation to temperature sensor

--- arduino/temperature/temperature.ino
+++ arduino/temperature/temperature.ino
@@ -1,6 +1,57 @@
+//
+// Basic ground temperature thermometer.
+// 
+// Configuration:
+// 
+// Analog input #0 is connected as follows;
+// 
+// 5v+ <---/\/\/ 5k ohm /\/\---> A0 <--- /\/\/ thermistor/\/\-> Ground 
+// 
+// The thermometer has two calibration phases.  They are performed as follows:
+//
+// 1. Power down the sensor
+// 2. Fill a glass half full with crused ice and top it off with
+//    water.  Allow to sit for 5 minutes.
+// 3. Immerse the temperature probe into the ice wate mixture
+// 4. Jumper 3.3V+ to Digital I/O #12
+// 5. Power on the sensor with battery power only and leave powered on
+//    until the LEDs stop flashing. This shouldf require about 5 minutes.
+// 6. When the LEDS stop flashing power off the sensor.
+// 7. Place the sensor under your arm
+// 8. Jumper 3.3V+ to Digital I/O #13 instead of #12
+// 9. Power on the sensor and wait until the flashing stops.  Thos
+//    step should require 5 minutes.
+// 10. Power down the sensor
+// 11. Remove all jumpers from #12 and #13
+//
+// Calibration is complete.
+//
+// Upon starting the sensor temperature is shown as the number of
+// quarter degrees above -20C.  This value is shown with a novel
+// encoding that permits the storage of values between [0, 239] to be
+// stored and decoded within 9 bits without abigioutly in the event of
+// reversal.
+// 
+// The following function is applied to the temperature value: The
+// temperature value is broken into two nibbles, the second bit
+// reversed with a single bit between them indicating which of the
+// nibbles is greater.  This is done to ensure the temepature can be
+// unabigiously decoded whether read from the sensor forward or
+// backward.
+// 
+// Prior to display the following function is applied to the
+// temperatuer to ensure the upper and lower nibbles are never the
+// same:
+//
+// F(t) = (t+16) * 17 / 16 - 16
+//
+// To deploy your sensor bury your thermocouple 8 inches under the
+// groun in an area of partial share near a tree.  Connect your
+// thermometer, power on and record the bit pattern into the WNYC
+// Cicada page.
+
 
 #include <EEPROM.h>
-
 
 #define THERMOMETER 0
 #define SET_LOW 12
@@ -8,155 +59,66 @@
 
 void setup()
 {
-  Serial.begin(19200);             // the Serial port of Arduino baud rate.
   pinMode(THERMOMETER, INPUT);
-
   
-  for(i=2;i<11;i++) {
+  for(i=2;i<11;i++) 
     pinMode(i, OUTPUT);
 
-  if (digitalRead(12)) {
-    Serial.println("Setting low temperature");
-    low_set = true;
-  }
-  if (digitalRead(13)) {
-    Serial.println("Setting high temperature");
-    high_set = true;
-  }
-  Serial.println("Starting up!");
+  pinMode(12, INPUT);
+  pinmode(13, INPUT);
 }
-  void setBit(int bit, boolean value) {
-    digitalWrite(bit + 2, value ? HIGH | LOW);
-  }
 
-  void all_on() {
-    int i;
-    for(i=0; i < 9;i++) {
-      setBit(i, True);
-    }
-  }
+void setBit(int bit, boolean value) {
+  digitalWrite(bit + 2, value ? HIGH | LOW);
+}
 
-  void all_off() {
-    int i;
-    for(i=0; i < 9; i++ ) {
-      setBit(i, False);
-    }
+void all_on() {
+  // Set all LEDs on.
+  int i;
+  for(i=0; i < 9;i++) {
+    setBit(i, True);
   }
+}
 
-  void write(unsigned byte i) {
-    unsigned byte display = 1;
-    if (i > 239)
-      return all_on();
-    
-    while(i-=1) {
-      for(display +=1;(((display >> 4) & 0x0f) == (display & 0x0f)); display += 1);
+void all_off() {
+  // Set all LEDs off.
+  int i;
+  for(i=0; i < 9; i++ ) {
+    setBit(i, False);
     }
+}
+
+void write(unsigned byte i) {
+  unsigned byte display = 1;
+  if (i > 239)
+    return all_on();
+
+  i = (i+16) * 17 / 16 - 16
 
     setBit(0, display & 1);
     setBit(1, display & 2);
     setBit(2, display & 4);
     setBit(3, display & 8);
     setBit(4, ((i >> 4) & 0x0f) > ( i & 0x0f));
-    setBit(5, display & 16);
-    setBit(6, display & 32);
-    setBit(7, display & 64);
-    setBit(8, display & 128);
+    setBit(5, display & 128);
+    setBit(6, display & 64);
+    setBit(7, display & 32);
+    setBit(8, display & 16);
   }
 
 void eeprom_write_int(int o, int i) {
-  Serial.print("eeprom_write: ");
-  Serial.println(i);
-  Serial.print("address: ");
-  Serial.print(o);
-  Serial.print(" gets ");
-  Serial.println( (i >> 8) & 0x3f );
+  // Write a signed integer into the EEPROM
   EEPROM.write(o, (i >> 8) & 0x3f);
-  Serial.print("address: ");
-  Serial.print(o+1);
-  Serial.print(" gets ");
-  Serial.println( i & 0xff );
   EEPROM.write(o+1, i & 0xff);
 }
 
 int eeprom_read_int(int i) {
+  // Read a signed integer from the EEPROM
   return EEPROM.read(i) << 8 | EEPROM.read(i+1);
 }
 
- const int samples=30;
-int temps[samples];
-void stablized_temperature_store(char *desc, int eeprom, int blink_rate) {
- 
-  int i, minimum, maximum,offset;
-  digitalWrite(THERMOMETER, HIGH);
-  delay(500);
-  for(i=0;i<samples;i+=1) {
-    if (i%2)
-      temps[i] = 1;
-    else
-      temps[i] = 1000;
-  }
 
-  offset=0;
-  minimum = 0;
-  maximum = 1000;
-  while ((maximum - minimum) > 5) {
-    minimum = temps[0];
-    maximum = temps[0];
-    for(i=1;i<samples;i++) {
-      if (temps[i] < minimum) minimum = temps[i];
-      if (temps[i] > maximum) maximum = temps[i];
-    }
-    temps[offset ] = (analogRead(SENSOR) + analogRead(SENSOR) +
-		      analogRead(SENSOR) + analogRead(SENSOR) +
-		      analogRead(SENSOR) + analogRead(SENSOR) +
-		      analogRead(SENSOR) + analogRead(SENSOR)+
-		      analogRead(SENSOR) + analogRead(SENSOR) +
-		      analogRead(SENSOR) + analogRead(SENSOR) +
-		      analogRead(SENSOR) + analogRead(SENSOR) +
-		      analogRead(SENSOR) + analogRead(SENSOR)) / 16;
-    Serial.print(desc);
-    Serial.print(offset);
-    Serial.print(" : ");
-    Serial.print(temps[offset]);
-    Serial.print(" [");
-    Serial.print(minimum);
-    Serial.print("-");
-    Serial.print(maximum);
-    Serial.println("]");
-
-    offset = (offset + 1 ) % samples;
-    for(i=0;i<blink_rate*3;i++) {
-
-      digitalWrite(LED, HIGH);
-      delay(500/blink_rate);
-      digitalWrite(LED, LOW);
-      delay(500/blink_rate);       
-    }
-  }
-  eeprom_write_int(eeprom, (temps[offset] + temps[(offset-1)%samples])/2);
-  digitalWrite(THERMOMETER, LOW);
-}
-
-void low_set_loop() {
-  Serial.println("low_set_loop();");
-  stablized_temperature_store("Establishing low temp:", 16, 1);
-  all_on();
-  while(1) delay(1000);
-
-
-
-}
-void high_set_loop() {
-  Serial.println("high_set_loop();");
-  stablized_temperature_store("Establishing high temp:", 18, 2);
-  while(1) delay(1000);
-  all_on();
-
-  }
-}
-
-
-int measure_temperature(int sensor) {
+int measure_temperature() {
   int i;
   sum = 0;
   for(i=0;i<16;i++)
@@ -164,6 +126,55 @@ int measure_temperature(int sensor) {
   sum /= 16;
   return sum;
 }
+
+void stablized_temperature_store(int eeprom, int blink_rate) {
+  // Wait until the temperaeture stablizes and set the eeprom.
+  const int samples=30;
+  const int threshold=5;
+  int temps[samples]; 
+  int i, minimum, maximum,offset;
+
+  for(i=0;i<samples;i+=2)
+    temps[i] = 0;
+  for(i=1;i<samples;i+=2)
+    temps[i] = 1023;
+
+  offset=0;
+  minimum = 0;
+  maximum = 1023;
+  while ((maximum - minimum) > threshold) {
+    minimum = temps[0];
+    maximum = temps[0];
+    for(i=1; i < samples; i++) {
+      if (temps[i] < minimum) 
+	minimum = temps[i];
+      if (temps[i] > maximum) 
+	maximum = temps[i];
+    }
+    temps[offset] = measure_temperature();
+    offset = (offset + 1 ) % samples;
+    for(i=0;i<blink_rate*3;i++) {
+      all_on();
+      delay(500/blink_rate);
+      all_off();
+      delay(500/blink_rate);       
+    }
+  }
+  eeprom_write_int(eeprom, (temps[offset] + temps[(offset-1)%samples])/2);
+}
+
+void low_set_loop() {
+  stablized_temperature_store("Establishing low temp:", 16, 1);
+  all_on();
+  while(1) delay(1000);
+}
+
+void high_set_loop() {
+  stablized_temperature_store("Establishing high temp:", 18, 2);
+  all_on();
+  while(1) delay(1000);
+}
+
 
 float temp_as_k(int value) {
   
@@ -187,7 +198,6 @@ void loop_main() {
   int listen_count;
 
 
-
   if ((EEPROM.read(16) == 255 && EEPROM.read(17)) ||
       (EEPROM.read(18) == 255 && EEPROM.read(19)) ) {
     digitalWrite(LED, HIGH);
@@ -204,31 +214,19 @@ void loop_main() {
     return; 
   }
     
-
   // Our LED array can emit values in the range [0, 239].  Our
   // temperature will be in units of 0.25 celcius with zero and -20c.  This
   // will give us a range from [-20, 39.75] celcius.
-  kelvin =temp_as_k(measure_temperature(SENSOR));
-  (273.15 + 20.0)) / 4.0 ;
-  temp = (
-
-  
-  Serial.print("temp: ");
-  Serial.println(temp);
-   
-
+  write_int((temp_as_k(measure_temperature()) + 20.0) / 4.0);
 }
 
 void loop() {
-  digitalWrite(AMP, LOW);
-  digitalWrite(THERMOMETER, LOW);
-
-  if (low_set) {
+  if (digitalRead(12))
     return low_set_loop();
-  } 
-  if (high_set) {
+ 
+  if (digitalRead(13))
     return high_set_loop();
-  }
+ 
   loop_main();
 }
 
